69eb7d137ff64ed4f92f9fb8f54bc1a505b7765a
Sergio Duran Barragan <sergio.duran@academicos.udg.mx>
2026-08-28 10:23:15 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mycalcium-v4';
+const CACHE='mycalcium-v8';
 const APP=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
